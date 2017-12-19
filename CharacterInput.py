f33532722000f099123121d894812fd1239142c9
williechow97 <willie.chow01@gmail.com>
2017-12-18 21:44:42 -0800

    Add files via upload

--- CharacterInput.py
+++ CharacterInput.py
@@ -2,12 +2,14 @@
 # Program that asks user for name and age. Prints out message that tells them when they will turn 100 years old 
 
 import datetime
+import re
 
 def main():
     userInput = input("Enter name, followed by age: ") 
-    (name, age) = userInput.split(' ')
-    #print( name)
+    m = re.search('([^\d]*)(\d+)',userInput)
+    print(m.group(1).rstrip())
     current = datetime.datetime.now()
-    #print(current.year)
-    print(name,'will turn 100 in the year', (int(current.year) + 100))
+    print(m.group(1),'will turn 100 in the year', (int(current.year) + (100 - int(m.group(2)))))
+
+
 main()
