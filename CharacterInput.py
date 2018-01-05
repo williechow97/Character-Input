cd1d0706fca8deadb71dc76451f1a67ca1722856
williechow97 <willie.chow01@gmail.com>
2018-01-04 16:42:41 -0800

    add comments to code

--- CharacterInput.py
+++ CharacterInput.py
@@ -6,10 +6,9 @@ import re
 
 def main():
     userInput = input("Enter name, followed by age: ") 
-    m = re.search('([^\d]*)(\d+)',userInput)
-    print(m.group(1).rstrip())
-    current = datetime.datetime.now()
-    print(m.group(1),'will turn 100 in the year', (int(current.year) + (100 - int(m.group(2)))))
+    m = re.search('([^\d]*)(\d+)',userInput) # use regex to split string -- first part characters, second part numbers
+    current = datetime.datetime.now() # get the current time
+    print(m.group(1).rstrip(),'will turn 100 in the year', (int(current.year) + (100 - int(m.group(2)))))
 
 
 main()
